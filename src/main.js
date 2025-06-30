aca94de30bec7575c45d3b2283e3ed409701cff0
Simone <>
2025-06-30 20:33:38 +0200

    about5

--- src/main.js
+++ src/main.js
@@ -61,7 +61,7 @@ const rotatingModels = [];
 const modelsData = [
   {
     path: "/models/katrin.glb",
-    desktop: { position: [-6, 1.6, 0], scale: [1.2, 1.2, 1.2] },
+    desktop: { position: [-5.8, 1.6, 0], scale: [1.2, 1.2, 1.2] },
     tablet: { position: [-2, 2, 0], scale: [1, 1, 1] },
     mobile: { position: [0, 1.6, 0], scale: [1, 1, 1] }
   },
@@ -69,17 +69,17 @@ const modelsData = [
     path: "/models/gio.glb",
     desktop: { position: [0, 1.6, 0], scale: [1.3, 1.3, 1.3] },
     tablet: { position: [2, 2, 0], scale: [1, 1, 1] },
-    mobile: { position: [0, -2.6, 0], scale: [1.1, 1.1, 1.1] }
+    mobile: { position: [0, -2, 0], scale: [1.1, 1.1, 1.1] }
   },
   {
     path: "/models/rebecca.glb",
-    desktop: { position: [6, 1.6, 0], scale: [1, 1, 1] },
+    desktop: { position: [5.8, 1.6, 0], scale: [1, 1, 1] },
     tablet: { position: [-2, 0, 0], scale: [1, 1, 1] },
-    mobile: { position: [0, -5.6, 0], scale: [0.8, 0.8, 0.8] }
+    mobile: { position: [0, -4, 0], scale: [0.8, 0.8, 0.8] }
   },
   {
     path: "/models/marta.glb",
-    desktop: { position: [-6, -2.4, 0], scale: [0.9, 0.9, 0.9] },
+    desktop: { position: [-5.8, -2.4, 0], scale: [0.9, 0.9, 0.9] },
     tablet: { position: [2, 0, 0], scale: [1, 1, 1] },
     mobile: { position: [0, -8.6, 0], scale: [0.7, 0.7, 0.7] }
   },
@@ -91,7 +91,7 @@ const modelsData = [
   },
   {
     path: "/models/fra.glb",
-    desktop: { position: [6, -2.4, 0], scale: [1, 1, 1] },
+    desktop: { position: [5.8, -2.4, 0], scale: [1, 1, 1] },
     tablet: { position: [2, -2, 0], scale: [1, 1, 1] },
     mobile: { position: [0, -14.6, 0], scale: [0.8, 0.8, 0.8] }
   }
